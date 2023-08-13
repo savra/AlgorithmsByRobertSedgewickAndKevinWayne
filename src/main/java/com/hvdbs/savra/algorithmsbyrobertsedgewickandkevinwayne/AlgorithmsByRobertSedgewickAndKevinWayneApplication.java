52c660ebfd48898b5d2f28640f75c52a78d899b7
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-13 16:27:50 +0300

    initial commit

---
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/AlgorithmsByRobertSedgewickAndKevinWayneApplication.java
@@ -0,0 +1,12 @@
+package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsByRobertSedgewickAndKevinWayneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsByRobertSedgewickAndKevinWayneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/AlgorithmsByRobertSedgewickAndKevinWayneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsByRobertSedgewickAndKevinWayneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
