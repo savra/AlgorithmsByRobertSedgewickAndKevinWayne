84ee2421e7cb5a3b38194147b2a8d04319dd1ba0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 15:35:57 +0300

    solution for exercise 1.1.8

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_08.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_08.java
@@ -5,11 +5,20 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_08 {
     public static void main(String[] args) {
+        System.out.print("A) ");
+        System.out.println('b');
 
+        System.out.print("B) ");
+        System.out.println('b' + 'c');
+
+        System.out.print("C) ");
+        System.out.println((char) ('a' + 4));
     }
 }
 
 /*
 Result:
-
+A) b
+B) 197
+C) e
 */
