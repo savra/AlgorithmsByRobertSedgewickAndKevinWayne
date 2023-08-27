81fd1b11cf20d15da36f259a00f8012296eaa200
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 17:42:58 +0300

    solution for exercise 1.1.10

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_10.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_10.java
@@ -5,11 +5,16 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_10 {
     public static void main(String[] args) {
+        /*
+        Error case from book (Variable 'a' might not have been initialized):
+        int[] a;
+        for (int i = 0; i < 10; i++)
+            a[i] = i * i;
 
+        Corrected version:
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = i * i;
+        */
     }
 }
-
-/*
-Result:
-
-*/
