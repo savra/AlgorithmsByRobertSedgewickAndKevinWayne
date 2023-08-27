f4de7a54a96e2ac4856a3ec5e000673a4d5a24a3
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 19:39:20 +0300

    solution for exercise 1.1.12

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_12.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_12.java
@@ -5,11 +5,26 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_12 {
     public static void main(String[] args) {
-
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i] ];
+        for (int i = 0; i < 10; i++)
+            System.out.println(i) ;
     }
 }
 
 /*
 Result:
-
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
 */
