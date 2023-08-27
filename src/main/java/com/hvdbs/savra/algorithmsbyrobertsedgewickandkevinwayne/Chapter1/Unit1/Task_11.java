f0cee177646ac7ab75868de2c82a42c36ed42022
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 19:29:54 +0300

    solution for exercise 1.1.11

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_11.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_11.java
@@ -5,7 +5,35 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_11 {
     public static void main(String[] args) {
+        boolean[][] arr = new boolean[][] {
+                {true,  true,  false, true},
+                {true,  false, true,  true},
+                {true,  true,  false, true},
+                {false, true,  false, false}
+        };
 
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                System.out.print("  ");
+
+                for (int k = 0; k < arr[0].length; k++) {
+                    System.out.print(k);
+
+                    if (k != arr[0].length - 1) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+            System.out.print(i + " ");
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
     }
 }
 
