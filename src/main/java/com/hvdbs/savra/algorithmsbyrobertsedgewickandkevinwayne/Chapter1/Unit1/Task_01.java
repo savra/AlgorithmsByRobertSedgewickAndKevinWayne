a3a779f3b08394ec147f2f28a9ac81573a4234bb
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 13:25:47 +0300

    solution for exercise 1.1.1

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_01.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_01.java
@@ -1,8 +1,19 @@
 package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 
-/**
- * ex. 1.1.1
- *
+/*
+  ex. 1.1.1
 */
 public class Task_01 {
+    public static void main(String[] args) {
+        System.out.println("A) " + (0 + 15) / 2);
+        System.out.println("B) " + 2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
 }
+
+/*
+Result:
+A) 7
+B) 200.0000002
+C) true
+*/
