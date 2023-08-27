46e25c15efd30d4e7dc8d79ab25902a4e054d732
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 17:35:39 +0300

    solution for exercise 1.1.9

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_09.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_09.java
@@ -5,9 +5,24 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_09 {
     public static void main(String[] args) {
+        System.out.println(toBinaryString(76));
+        System.out.println(toBinaryString(77));
+        System.out.println(toBinaryString(145));
+        System.out.println(toBinaryString(763));
+    }
 
+    private static String toBinaryString(int N) {
+        StringBuilder sb = new StringBuilder();
+
+        while(N > 0) {
+            sb.insert(0, (N % 2));
+            N /= 2;
+        }
+
+        return sb.toString();
     }
 }
+// 72
 
 /*
 Result:
