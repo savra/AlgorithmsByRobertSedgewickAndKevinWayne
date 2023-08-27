b0f0296a57616a030e620177d2045a18f77c3e3d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 14:19:52 +0300

    solution for exercise 1.1.4

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_04.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_04.java
@@ -5,7 +5,10 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_04 {
     public static void main(String[] args) {
-
+        System.out.println("A) if (a > b) then c = 0; - There is no keyword \"then\" in java");
+        System.out.println("B) if a > b { c = 0; } - Missing parentheses after \"if\"");
+        System.out.println("C) if (a > b) c = 0; - No errors");
+        System.out.println("D) if (a > b) c = 0 else b = 0; - Missing semicolon after \"c = 0\"");
     }
 }
 
