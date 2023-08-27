510ed9263e3002c4ec925ce38e4f72487403db09
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-27 15:19:54 +0300

    solution for exercise 1.1.6

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_06.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_06.java
@@ -1,15 +1,40 @@
 package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 
+import edu.princeton.cs.algs4.StdOut;
+
 /*
   ex. 1.1.6
 */
 public class Task_06 {
     public static void main(String[] args) {
+        int f = 0;
+        int g = 1;
 
+        for (int i = 0; i <= 15; i++)
+        {
+            StdOut.println(f);
+            f = f + g;
+            g = f - g;
+        }
     }
 }
 
 /*
-Result:
-
+Result (Is Fibonacci sequence):
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
 */
