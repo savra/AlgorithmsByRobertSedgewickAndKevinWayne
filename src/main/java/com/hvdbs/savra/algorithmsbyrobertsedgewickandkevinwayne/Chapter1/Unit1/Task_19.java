2919f04ab8c9abec81c24911c397fc290a038ff2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-10 17:22:21 +0300

    solution for exercise 1.1.19

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_19.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_19.java
@@ -2,7 +2,7 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 
 import edu.princeton.cs.algs4.StdOut;
 
-import static com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1.Fibonacci.F;
+import static com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1.Task_19.Fibonacci.F;
 
 /*
   ex. 1.1.19
@@ -13,27 +13,27 @@ public class Task_19 {
             StdOut.println(N + " " + F(N));
         }
     }
-}
 
-class Fibonacci {
-    public static long F(int N) {
-        if (N == 0) {
-            return 0;
+    static class Fibonacci {
+        public static long F(int N) {
+            if (N == 0) {
+                return 0;
+            }
+
+            if (N == 1) {
+                return 1;
+            }
+
+            long[] result = new long[N + 1];
+            result[0] = 0;
+            result[1] = 1;
+
+            for (int i = 2; i <= N; i++) {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+
+            return result[N];
         }
-
-        if (N == 1) {
-            return 1;
-        }
-
-        long[] result = new long[N + 1];
-        result[0] = 0;
-        result[1] = 1;
-
-        for (int i = 2; i <= N; i++) {
-            result[i] = result[i - 1] + result[i - 2];
-        }
-
-        return result[N];
     }
 }
 
