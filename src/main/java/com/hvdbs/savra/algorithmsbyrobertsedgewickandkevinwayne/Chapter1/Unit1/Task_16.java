e5ff1b9f340f926f36ba7c6098e20bdbe9626346
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-10 15:48:33 +0300

    solution for exercise 1.1.16

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_16.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_16.java
@@ -5,11 +5,19 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_16 {
     public static void main(String[] args) {
+        System.out.println(exR1(6));
+    }
 
+    public static String exR1(int n) {
+        if (n <= 0) {
+            return "";
+        }
+
+        return exR1(n - 3) + n + exR1(n - 2) + n;
     }
 }
 
 /*
 Result:
-
+311361142246
 */
