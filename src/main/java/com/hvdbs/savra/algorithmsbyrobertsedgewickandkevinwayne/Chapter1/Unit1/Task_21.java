6f1496fb080ef7a7b79079dcc99acdd75e09b706
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-10 17:52:56 +0300

    solution for exercise 1.1.21

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_21.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_21.java
@@ -1,15 +1,25 @@
 package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 
+import edu.princeton.cs.algs4.StdIn;
+
 /*
   ex. 1.1.21
 */
 public class Task_21 {
     public static void main(String[] args) {
+        String in = StdIn.readLine();
+        String[] strings = in.split(" ");
+        String name = strings[0];
+        int firstDigit = Integer.parseInt(strings[1]);
+        int secondDigit = Integer.parseInt(strings[2]);
+        double result = (double) firstDigit / secondDigit;
 
+        System.out.printf("%s %d %d %.3f", name, firstDigit, secondDigit, result);
     }
 }
 
 /*
 Result:
-
+John 24 3
+John 24 3 8.000
 */
