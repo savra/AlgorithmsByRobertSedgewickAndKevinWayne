5c8bebb853dd852b2ef6a6e6613b2e4789df86a0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-11 18:02:20 +0300

    solution for exercise 1.1.22

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_15.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_15.java
@@ -5,7 +5,7 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_15 {
     public static void main(String[] args) {
-        int[] source = new int[] {1, 1, 2, 1, 3, 4, 5, 5, 5, 6, 4, 2, 1};
+        int[] source = new int[]{1, 1, 2, 1, 3, 4, 5, 5, 5, 6, 4, 2, 1};
         int[] histogram = histogram(source, 9);
 
         for (int i : histogram) {
