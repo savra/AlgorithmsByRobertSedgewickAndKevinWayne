b3a4f4e971f8ee5e17f74a70eeab61cc5f8d8c2a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-11 19:57:35 +0300

    solution for exercise 1.1.26

--- src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_26.java
+++ src/main/java/com/hvdbs/savra/algorithmsbyrobertsedgewickandkevinwayne/Chapter1/Unit1/Task_26.java
@@ -5,11 +5,41 @@ package com.hvdbs.savra.algorithmsbyrobertsedgewickandkevinwayne.Chapter1.Unit1;
 */
 public class Task_26 {
     public static void main(String[] args) {
+        sort(1,3, 2, 5);
+        System.out.println();
+        sort(4,1, 2, 3);
+        System.out.println();
+        sort(2,4, 1, 3);
+    }
 
+    private static void sort(int a, int b, int c, int t) {
+
+        System.out.println("Before sort: a = " + a + " b = " + b + " c = " + c);
+        if (a > b) {
+            t = a;
+            a = b;
+            b = t;
+        }
+
+        if (a > c) {
+            t = a;
+            a = c;
+            c = t;
+        }
+
+        if (b > c) {
+            t = b;
+            b = c;
+            c = t;
+        }
+
+        System.out.println("After sort: a = " + a + " b = " + b + " c = " + c);
     }
 }
 
 /*
 Result:
-
+a = 1 b = 2 c = 3
+a = 1 b = 2 c = 4
+a = 1 b = 2 c = 4
 */
